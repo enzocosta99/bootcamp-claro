2ed288a625788c97601c968eef5ebe5c4ae72494
Enzo Costa <enzo99costa@gmail.com>
2024-10-02 09:19:13 -0300

    Adicionando classe utilizada para o estudo de tipos primitivos, variáveis e constantes

--- trilha_java_basico/tipos-e-variaveis/src/TiposVariaveis.java
+++ trilha_java_basico/tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        double salarioMinimo = 2.500; // Mesma coisa que 2,5 ou 2.5, e não 2500
+
+        short numeroCurto = 1;
+        int numeroCurto1 =  numeroCurto;
+        short numeroCurto2 = (short) numeroCurto1;
+
+        // Variáveis: valores que podem ser mudados
+        int numero = 5;
+        numero = 10;
+
+        // Constantes: valores que não podem ser mudados
+        //obs: devem ser declaradas em maiúscular e as palavras que a compõem devem ser separadas por sublinhados
+        final double VALOR_DE_PI = 3.14;
+        //VALOR_DE_PI = 3.15;
+
+        
+
     }
 }
